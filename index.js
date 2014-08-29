fc19c95d33d036a61339d1bb53da27477d3567dc
Dan Flettre <fletd01@yahoo.com>
2014-08-28 20:33:43 -0500

    make it so

---
+++ index.js
@@ -0,0 +1,27 @@
+var Hapi = require('hapi');
+
+var server = Hapi.createServer('localhost', process.env.PORT || 8080);
+
+server.pack.register({
+    plugin: require('revisit-mutagen'),
+    options: {
+        'trippyshift': require('trippyshift'),
+        'butts': require('butts-gm'),
+        'echoplease': function (buffer, callback) {
+            // you can just write your own "mutator" inline too!
+            callback(null, buffer);
+        }
+    }
+}, function (err) {
+    if (err) throw err;
+    server.start(function () {
+
+        // list out all the routes for verification
+        server.table().forEach(function (row) {
+            console.log(server.info.uri + row.path + " (" + row.method + ")");
+        });
+
+        console.log("technodrome server started @", server.info.uri);
+    });
+
+});
