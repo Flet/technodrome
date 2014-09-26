4010c3855c667816d6c9dc9d06326a04d273a242
Dan Flettre <fletd01@yahoo.com>
2014-09-26 18:03:50 -0500

    add .eslintrc and .npmignore and minor linting updates

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var Hapi = require('hapi');
+var Hapi = require('hapi')
 
-var server = Hapi.createServer('0.0.0.0', process.env.PORT || 8080);
+var server = Hapi.createServer('0.0.0.0', process.env.PORT || 8080)
 
 server.pack.register({
     plugin: require('revisit-mutagen'),
     options: {
         moreSamples: ['face.jpg'],
         glitches: {
-            'trippyshift': require('trippyshift'),
-            'butts': require('butts-gm'),
-            'echoplease': function(buffer, callback) {
-                // you can just write your own "mutator" inline too!
-                callback(null, buffer);
+            trippyshift: require('trippyshift'),
+            butts: require('butts-gm'),
+            echoplease: function(buffer, callback) {
+                // you can just write your own 'mutator' inline too!
+                callback(null, buffer)
             }
         }
     }
 }, function(err) {
-    if (err) throw err;
+    if (err) throw err
     server.start(function() {
 
         // list out all the routes for verification
         server.table().forEach(function(row) {
-            console.log(server.info.uri + row.path + " (" + row.method + ")");
-        });
+            console.log(server.info.uri + row.path + ' (' + row.method + ')')
+        })
 
-        console.log("technodrome server started @", server.info.uri);
-    });
+        console.log('technodrome server started @', server.info.uri)
+    })
 
-});
+})
